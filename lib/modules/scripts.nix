0e2a355ebc3a0e215cf02c334d4ca2755cea48cb
Emma <contact@emma.pizza>
2025-07-01 11:07:38 +0200

    scripts: package using module system

---
+++ lib/modules/scripts.nix
@@ -0,0 +1,78 @@
+{ runCommandNoCC
+, makeWrapper
+, types
+, lib }:
+let
+  inherit (types)
+    submodule
+    attrsOf
+    package
+    listOf
+    path
+    str
+    ;
+  inherit (lib)
+    concatStringsSep
+    mapAttrsToList
+    makeBinPath
+    evalModules
+    mapAttrs
+    mkOption
+    pipe
+    ;
+
+  baseModule = {
+    options.scripts = mkOption {
+      type = pipe ({ name, ... }: {
+        options = {
+          src = mkOption {
+            type = path;
+            default = ../../own/scripts/bin/${name}.bash;
+          };
+          aliases = mkOption {
+            type = listOf str;
+            default = [];
+            apply = concatStringsSep " ";
+          };
+          paths = mkOption {
+            type = listOf package;
+            default = [];
+            apply = makeBinPath;
+          };
+          substitutions = mkOption {
+            type = attrsOf str;
+            default = {};
+          };
+        };
+      }) [ submodule attrsOf ];
+      apply = mapAttrs (name: { src, paths, aliases, substitutions }:
+        runCommandNoCC "${name}-script"
+          { nativeBuildInputs = [ makeWrapper ]; }
+          ''
+            mkdir -p "$out"/bin
+            ${if substitutions == {} then ''
+              install -m755 "${src}" "$out"/bin/"${name}"
+            '' else ''
+              substitute "${src}" "$out"/bin/"${name}" \
+              ${pipe substitutions [ (mapAttrsToList (key: replacement: ''
+                --subst-var-by "${key}" "${replacement}"
+              '')) (concatStringsSep "\\\n") ]}
+              chmod 755 "$out"/bin/"${name}"
+            ''}
+            patchShebangs "$out"/bin/"${name}"
+            if [ -n "${paths}" ]; then
+              wrapProgram "$out"/bin/"${name}" --prefix PATH : "${paths}"
+            fi
+            if [ -n "${aliases}" ]; then
+              for i in ${aliases}; do
+                ln -s "${name}" "$out"/bin/"$i"
+              done
+            fi
+          ''
+      );
+    };
+  };
+in {
+  defsModule = baseModule;
+  evalDefs = x: (evalModules { modules = [ baseModule x ]; }).config.scripts;
+}
--- own/scripts/default.nix
+++ own/scripts/default.nix
@@ -1,64 +1,42 @@
 { pkgs, lib, ... }:
 let
-  inherit (lib)
-    makeBinPath
-    ;
+  scriptsModule = import ../../lib/modules/scripts.nix {
+    inherit (pkgs) runCommandNoCC makeWrapper;
+    inherit (lib) types;
+    inherit lib;
+  };
+  
   inherit (pkgs)
-    runCommandNoCC
-    gnused
     util-linux
-    makeWrapper
-    gnutar
-    gnugrep
     findutils
     usbutils
+    gnugrep
+    gnused
     ;
-
-  basicScript =
-    name:
-    runCommandNoCC name
-      {
-      }
-      ''
-        mkdir -p $out/bin
-        install -m755 ${./bin/${name}.bash} $out/bin/${name}
-        patchShebangs $out/bin/${name}
-      '';
-  pathScript =
-    name: paths:
-    runCommandNoCC name
-      {
-        nativeBuildInputs = [ makeWrapper ];
-      }
-      ''
-        mkdir -p $out/bin
-        install -m755 ${./bin/${name}.bash} $out/bin/${name}
-        patchShebangs $out/bin/${name}
-        wrapProgram $out/bin/${name} --prefix PATH : "${makeBinPath paths}"
-      '';
-in
-{
-  clean-old-gens = basicScript "clean-old-gens";
-  gen-machine-id = pathScript "gen-machine-id" [ util-linux ];
-  nixpkgs-grep =
-    runCommandNoCC "nixpkgs-grep"
-      {
-        nativeBuildInputs = [ makeWrapper ];
-        nixpkgs = pkgs.path;
-      }
-      ''
-        mkdir -p $out/bin
-        substitute ${./bin/nixpkgs-grep.bash} $out/bin/nixpkgs-grep --subst-var nixpkgs
-        patchShebangs $out/bin/nixpkgs-grep
-        chmod 755 $out/bin/nixpkgs-grep
-        wrapProgram $out/bin/nixpkgs-grep --prefix PATH : "${makeBinPath [ gnugrep ]}"
-        ln -s nixpkgs-grep $out/bin/ng
-      '';
-  nr = basicScript "nr";
-  publicip = basicScript "publicip";
-  fhs-shell = basicScript "fhs-shell";
-  sandbox-shell = basicScript "sandbox-shell";
-  iommu-groups = pathScript "iommu-groups" [ findutils ];
-  usb-controllers = pathScript "usb-controllers" [ usbutils ];
-  fix-plasma-icons = pathScript "fix-plasma-icons" [ gnused ];
+in scriptsModule.evalDefs {
+  scripts = {
+    run-long-command = {};
+    clean-old-gens = {};
+    gen-machine-id = { paths = [ util-linux ]; };
+    nixpkgs-grep = {
+      substitutions = {
+        "nixpkgs" = "${pkgs.path}";
+      };
+      aliases = [ "ng" ];
+      paths = [ gnugrep ];
+    };
+    nr = {};
+    publicip = {};
+    fhs-shell = {};
+    sandbox-shell = {};
+    iommu-groups = {
+      paths = [ findutils ];
+    };
+    usb-controllers = {
+      paths = [ usbutils ];
+    };
+    fix-plasma-icons = {
+      paths = [ gnused ];
+    };
+  };
 }
