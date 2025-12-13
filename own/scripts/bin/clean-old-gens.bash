6ce870c3b26026d4c42c181ba8c1123946f21ffa
Emma <contact@emma.pizza>
2025-12-13 22:08:46 +0100

    clean-old-gens: tweaks

--- own/scripts/bin/clean-old-gens.bash
+++ own/scripts/bin/clean-old-gens.bash
@@ -8,14 +8,14 @@ spinner() {
     log="$(mktemp -t log.XXXXX)"
     ("$@" &>"$log") &
     proc=$!
-    while [ -e /proc/$proc ]; do
+    while [[ -e /proc/$proc ]]; do
         local i=$(date +%s) chars=("|" "/" "-" "\\")
         (( i %= 4 ))
         echo -ne "\r ${chars[i]} [$@] $(tail -n1 "$log")\033[0K"
         sleep .1s
     done
     wait "$proc"
-    if [ $? -gt 0 ]; then
+    if [[ $? -gt 0 ]]; then
         echo -e "\rerrored [$@]\033[0K"
     else
         echo -e "\rcompleted [$@]\033[0K"
@@ -23,16 +23,15 @@ spinner() {
     rm -f "$log"
 }
 
-# shellcheck disable=SC2086
-[ "$(id -u)" -ne 0 ] && exec sudo "$0" "$@"
+[[ $(id -u) -ne 0 ]] && exec run0 "$0" "$@"
 
 echo -ne "\033[0m\033[?7l"
 
 IFS=$'\n' read -r -d '' -a usersList < <(getent shadow | awk -F':' '$2 != "!" {print$1}' | uniq)
-if [ -n "${usersList[*]}" ]; then
+if [[ -n "${usersList[*]}" ]]; then
     echo "running garbage collection for users ( ${usersList[@]} )..."
     for i in "${usersList[@]}"; do
-        spinner sudo -u "$i" nix-collect-garbage -d
+        spinner su "$i" -lc 'nix-collect-garbage -d'
     done
 fi
 
