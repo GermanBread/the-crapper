20e9b6006274f50297f61b463d7757c83490a405
Emma <contact@emma.pizza>
2026-01-05 11:48:26 +0100

    remove fetchcord

--- foreign/default.nix
+++ foreign/default.nix
@@ -3,7 +3,6 @@ args: {
   kurve = import ./kurve args;
   fmlock = import ./fmlock args;
   queercat = import ./queercat args;
-  fetchcord = import ./fetchcord args;
   waydroid_script = import ./waydroid_script args;
   kwin4_effect_geometry_change = import ./kwin4_effect_geometry_change args;
 }
