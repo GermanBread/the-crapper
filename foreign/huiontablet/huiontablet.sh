0952fb5bee6368162ce0903ddd46d7377efb35bf
Emma <contact@emma.pizza>
2026-04-12 16:21:29 +0200

    huiontablet: allow a lot of X11 permissions

--- foreign/huiontablet/huiontablet.sh
+++ foreign/huiontablet/huiontablet.sh
@@ -32,6 +32,12 @@ ffmpeg -loglevel error -y -i <(spectacle -n -i -f -b -o /dev/stdout) -vf scale="
 kwriteconfig6 --file kcminputrc \
     --group Libinput --group 9580 --group 61165 --group 'HUION 256C PEN STYLUS' \
     --key MapToWorkspace --type bool true --notify &
+kwriteconfig6 --file kwinrc \
+    --group Xwayland --key XwaylandEavesdrops All --notify &
+kwriteconfig6 --file kwinrc \
+    --group Xwayland --key XwaylandEavesdropsMouse --type bool true --notify &
+kwriteconfig6 --file kwinrc \
+    --group Xwayland --key XwaylandEisNoPrompt --type bool true --notify &
 
 wait
 
