25cd7f2cf056182a190041eba348098c65a4b572
Emma <contact@emma.pizza>
2026-05-29 16:47:12 +0200

    kwin4_effect_geometry_change superceded by nixpkgs

--- default.nix
+++ default.nix
@@ -1,5 +1,4 @@
 { callPackages, callPackage, ... }: {
-  kwin4_effect_geometry_change = callPackage ./pkgs/kwin4_effect_geometry_change {};
   writeJavaScript = callPackage ./pkgs/writeJavaScript {};
   huiontablet = callPackage ./pkgs/huiontablet {};
   scripts = callPackages ./scripts {};
